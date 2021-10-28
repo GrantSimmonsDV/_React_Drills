748a33cc0b62436cd05388b46ae08a1f8b965519
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-10-27 20:29:05 -0600

    3.1

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [list, setList] = useState(["cheese", "milk", "bread", "cereal", "yogurt"]);
+
+
+const groceries = list.map((element, index) => {
+  return <h2 key={index} >{element}</h2>;
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="text" 
+     onChange = {(e) => setList(e.target.value)} />
+     
     </div>
   );
 }
