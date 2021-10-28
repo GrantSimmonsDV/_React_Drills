377ed59e3d2d7795a1b0de112c00f4c2901a7930
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-10-28 16:28:09 -0600

    3.2

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-const [list, setList] = useState(["cheese", "milk", "bread", "cereal", "yogurt"]);
+  const [list, setList] = useState([
+    "cheese",
+    "milk",
+    "bread",
+    "cereal",
+    "yogurt",
+  ]);
+  const [search, setSearch] = useState("");
 
+  // function handleChange(filter) {
+  //   setList({ list: filter });
+  // }
 
-const groceries = list.map((element, index) => {
-  return <h2 key={index} >{element}</h2>;
-});
+  // let groceries = list.filter((element, index) => {
+  //   return element.includes(list);
+  // });
+
+  // let groceries = list;
 
   return (
     <div className="App">
-     <input type="text" 
-     onChange = {(e) => setList(e.target.value)} />
-     
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
+      {list
+        .filter((element) => {
+          if (list == "") {
+            return element;
+          } else if (element.toLowerCase().includes(search.toLowerCase())) {
+            return element;
+          }
+        })
+        .map((element, index) => {
+          return <h2 key={index}>{element}</h2>;
+        })}
+
+      {list}
     </div>
   );
 }
