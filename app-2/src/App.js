ebce7b3ec8a67f93c5fa965d5a7cb87cd62cabbd
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-11-08 14:34:42 -0700

    updates

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -4,25 +4,16 @@ import "./App.css";
 function App() {
   //Variables
   const [arr, setArr] = useState(["Hank", "Bank", "Drank", "Stank"]);
+
   // Function
-  // setArr(){
-  //   let arr = arr;
-  //   let List = "";
-
-  //   List = arr.toString()
-  //   // for (let i = 0; i<arr.length; i++){
-  //   //   List += arr[i];
-  //   }
-
   let display = arr.map((element, index) => {
     return <h2 key={index}>{element}</h2>;
   });
+
   //Return
   return (
     <div className="App">
-    
-        <ul>{display}</ul>
- 
+      <ul>{display}</ul>
     </div>
   );
 }
--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 function App() {
-
-
   return (
     <div className="App">
       <Login />
-
-
     </div>
   );
 }
